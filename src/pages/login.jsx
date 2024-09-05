80a4af3359e511b57cf3f9495cd9eb7178ca21a5
bashtray <dajhely.angles.la@gmail.com>
2024-09-05 13:47:50 -0500

    Add formik validation to login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -92,6 +92,70 @@ const Form = styled('form')`
 	padding: 0 1em;
 `;
 
+const InputField = styled('div')`
+	/* text-align: start; */
+`;
+
+const ErrorMessage = styled('div')`
+	font-weight: 300;
+	color: #fff;
+	font-size: 0.8em;
+	text-align: start;
+`;
+
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+// formik handle form values, validation,and error messages
+
+// validate must recibe the obj values and return and obj with errors
+const validate = values => {
+	const errors = {}
+
+	for (const prop in values) {
+		// formik manual validation
+		switch (prop) {
+			case 'email':
+				if (values[prop].length <= 0) {
+					errors[prop] = `${capitalize(prop)} required`
+				} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values[prop])) {
+					errors[prop] = 'Invalid email address'
+				}
+				break;
+			
+				case 'password':
+				if (!values[prop]) {
+					errors[prop] = `${capitalize(prop)} required`
+				} else if (values[prop].length < 8) {
+					// must have minimun 8 characteres
+					errors[prop] = `${capitalize(prop)} must be 8 characters minimun`
+				} else if (!values[prop].match(/^[A-Z]*/)) {
+					// must have a capitalize letter
+					errors[prop] = `${capitalize(prop)} must have at least an upper character`
+				} else if (!values[prop].match(/[^\w\*]/)) {
+					// must have an special character
+					errors[prop] = `${capitalize(prop)} must have at least an special character`
+				} else if (!values[prop].match(/^[0-9]*/)) {
+					// must have a number
+					errors[prop] = `${capitalize(prop)} must contain a number`
+				}
+				console.log(errors[prop], 'err');
+
+				break
+			default:
+				break;
+		}
+		console.log(errors, 'errors');
+
+
+		// return !values[prop] ? errors[prop] = true : errors[prop] = false;
+	}
+	return errors;
+
+}
+
+
 function Login(params) {
 	// call formik
 	// define initial values
@@ -102,6 +166,8 @@ function Login(params) {
 			email: '',
 			password: ''
 		},
+		validate,
+		// call validate fn
 		onSubmit: ((values) => {
 			console.log('values: ', values)
 		})
@@ -122,7 +188,7 @@ function Login(params) {
 				</ContainerLogo>
 
 				<ContainerForm>
-					<ContainerText>	
+					<ContainerText>
 						<Title>Task Management &
 							To-Do List</Title>
 						<Subtitle>This productive tool is designed to help
@@ -133,12 +199,24 @@ function Login(params) {
 						<Container>
 							<Row>
 								<Label>Email</Label>
-								<Input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+								{/* input field */}
+								<InputField>
+									<Input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+									{/* error message label */}
+									{
+										formik.errors.email ? <ErrorMessage>{formik.errors.email}</ErrorMessage> : null
+									}
+								</InputField>
 							</Row>
 
 							<Row>
 								<Label>Password</Label>
-								<Input type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
+								<InputField>
+									<Input type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
+									{
+										formik.errors.password ? <ErrorMessage>{formik.errors.password}</ErrorMessage> : null
+									}
+								</InputField>
 							</Row>
 							<RowItemRight>
 								<Button type="submit">Log in!</Button>
@@ -153,3 +231,13 @@ function Login(params) {
 	)
 }
 export default Login
+
+
+// To-Do
+// [/] Add form validations using formik
+// Separate components from main view
+// import components
+
+// Nice to do
+// Work on responsivness (tablet/desktop)
+// change theme to ligth
